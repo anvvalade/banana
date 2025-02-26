646e85e559aaa312d61f6ce4621dd1ab3e363331
Aurélien Valade <valade@cppm.in2p3.fr>
2025-02-26 11:24:22 +0100

    first (again)

--- banana/run_sanity_check.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-from tensorphy import FFT3DWrapper, LogServer
-
-LogServer.cannot_save_error = False
-
-
-FFT3D = FFT3DWrapper(4, 1, verbosity=3)
-FFT3D.test()
--- banana/scripts/devel/remove_run.py
+++
@@ -1,4 +0,0 @@
-import sys
-from shutil import rmtree
-
-
